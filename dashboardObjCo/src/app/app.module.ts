c006557fe6249909efe83af97d1a7dcf503452dd
canneberge <kevfyrezz@gmail.com>
2020-05-04 16:28:48 +0200

    [WIP] logincompo front

--- dashboardObjCo/src/app/app.module.ts
+++ dashboardObjCo/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginCompoComponent } from './login-compo/login-compo.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { LoginCompoComponent } from './login-compo/login-compo.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
